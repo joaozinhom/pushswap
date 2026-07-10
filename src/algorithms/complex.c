a3288c094ee4794b440498121736b53b2d54b357
Joao Moreno Cruz Rosa <joamoren@c3r8p1.42sp.org.br>
2026-07-10 11:53:46 -0300

    fix complex

--- src/algorithms/complex.c
+++ src/algorithms/complex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   complex.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpinto-l <mpinto-l@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joamoren <joamoren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/10 09:29:13 by mpinto-l          #+#    #+#             */
-/*   Updated: 2026/07/10 09:29:42 by mpinto-l         ###   ########.fr       */
+/*   Updated: 2026/07/10 11:44:55 by joamoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,68 +20,80 @@ void	get_to_index(t_node **stack, int index)
 	pos = index_of(*stack, index);
 	size = stack_size(*stack);
 	if (pos <= size / 2)
+	{
 		while (pos-- > 0)
 			ra(stack);
+	}
 	else
-		while (pos++ < size)
+	{
+		pos = size - pos;
+		while (pos-- > 0)
 			rra(stack);
+	}
 }
 
 int	find_pivot(t_node *stack, int size)
 {
-	int	i;
 	int	min;
 	int	max;
+	int	i;
 
-	i = 0;
 	min = stack->value;
 	max = stack->value;
-	while (i < size)
+	i = 0;
+	while (i++ < size)
 	{
 		if (stack->value < min)
 			min = stack->value;
 		if (stack->value > max)
 			max = stack->value;
 		stack = stack->next;
-		i++;
 	}
 	return ((min + max) / 2);
 }
 
 int	partition(t_node **stack_a, t_node **stack_b, int size)
 {
-	int	pivot_value;
+	int	pivot;
+	int	pushed;
+	int	rotated;
 	int	i;
-	int	count_below;
 
-	pivot_value = find_pivot(*stack_a, size);
+	pivot = find_pivot(*stack_a, size);
+	pushed = 0;
+	rotated = 0;
 	i = 0;
-	count_below = 0;
-	while (i < size)
+	while (i++ < size)
 	{
-		if ((*stack_a)->value < pivot_value)
+		if ((*stack_a)->value < pivot)
 		{
 			pb(stack_b, stack_a);
-			count_below++;
+			pushed++;
 		}
 		else
+		{
 			ra(stack_a);
-		i++;
+			rotated++;
+		}
 	}
-	return (count_below);
+	while (rotated-- > 0)
+		rra(stack_a);
+	return (pushed);
 }
 
 void	quick_sort(t_node **stack_a, t_node **stack_b, int size)
 {
-	int	count_below;
+	int	pushed;
 	int	i;
 
 	if (size <= 1)
 		return ;
-	count_below = partition(stack_a, stack_b, size);
-	quick_sort(stack_a, stack_b, size - count_below);
-	i = count_below;
+	pushed = partition(stack_a, stack_b, size);
+	if (pushed == 0 || pushed == size)
+		return ;
+	quick_sort(stack_a, stack_b, size - pushed);
+	i = pushed;
 	while (i-- > 0)
 		pa(stack_a, stack_b);
-	quick_sort(stack_a, stack_b, count_below);
+	quick_sort(stack_a, stack_b, pushed);
 }
